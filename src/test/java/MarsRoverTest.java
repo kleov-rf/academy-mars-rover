5bb9a4f975d96f1b887ab6d5d7e46def7c57cab0
K Leonardo Ruiz Flórez <kleonardoruizflorez@gmail.com>
2023-05-25 15:17:52 +0200

    Added failing test

--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -72,6 +72,7 @@ public class MarsRoverTest {
     @ParameterizedTest
     @CsvSource({
             "MMMRRM, 0:2:S",
+            "MMMRRMM, 0:1:S",
     })
     void should_be_able_to_move_south(String input, String expectedOutput) {
         MarsRover rover = new MarsRover();
